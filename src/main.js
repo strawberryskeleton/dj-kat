45c775e116556359d7183d65a0cee26ed4ff0a99
StrawberrySkeleton <xradhi5685@gmail.com>
2026-07-14 14:13:17 +0530

    transparency & draggable fix: disabled window shadow + can move app with clicking anywhere on the app area (on img+text as well)

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
 const { invoke } = window.__TAURI__.core;
 
+window.addEventListener('dragover', (e) => {
+  e.preventDefault();
+});
 
-import { getCurrentWindow } from '@tauri-apps/api/window';
-
-const appWindow = getCurrentWindow();
-
-document.querySelector('main').addEventListener('mousedown', (e) => {
-  // Only drag on left click and ensure they didn't click a button inside it
-  if (e.buttons === 1 && e.target.tagName !== 'BUTTON') {
-    appWindow.startDragging();
-  }
+window.addEventListener('drop', (e) => {
+  e.preventDefault();
 });
