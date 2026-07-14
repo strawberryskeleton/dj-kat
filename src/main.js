50f892d61874dbade5d4226183e4c37b85ef57c0
StrawberrySkeleton <xradhi5685@gmail.com>
2026-07-14 13:52:01 +0530

    made app window transparent and draggable + adjusted window size to remain fixed as per my ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 const { invoke } = window.__TAURI__.core;
 
 
-window.addEventListener("DOMContentLoaded", () => {
- 
+import { getCurrentWindow } from '@tauri-apps/api/window';
+
+const appWindow = getCurrentWindow();
+
+document.querySelector('main').addEventListener('mousedown', (e) => {
+  // Only drag on left click and ensure they didn't click a button inside it
+  if (e.buttons === 1 && e.target.tagName !== 'BUTTON') {
+    appWindow.startDragging();
+  }
 });
